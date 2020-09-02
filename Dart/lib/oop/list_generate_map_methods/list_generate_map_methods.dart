dea0f69dafabcbaec94e509a0f554e50783e01a0
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-09-02 20:40:20 +0300

    generate and mapping methods examples

--- Dart/lib/oop/list_generate_map_methods/list_generate_map_methods.dart
+++ Dart/lib/oop/list_generate_map_methods/list_generate_map_methods.dart
@@ -1,34 +1,51 @@
 import 'dart:math';
 
 main(List<String> args) {
-
   List<int> studentsNo = List.filled(30, 0); // 30 numbers and all of them 0.
-  List<int> studentsNo2 = List.generate(30, (index) => index );// generated array between 0-30
-  List<int> studentsNo3 = List.generate(30, (index) => randomNo());// generated array between 0-60 randomly
+  List<int> studentsNo2 =
+      List.generate(30, (index) => index); // generated array between 0-30
+  List<int> studentsNo3 = List.generate(
+      30, (index) => randomNo()); // generated array between 0-60 randomly
+/*
+  List<Student> allStudents = studentsNo3.map((stuNo) {
+    return Student("Student number: $stuNo name", stuNo);
+  }).toList();
+*/
 
-  List<Student> allStudents = studentsNo3.map((stuNo){
-    return Student;
-  }).toList("Student number: $stuNo name", stuNo);
+  List<Student> allStudents = studentsNo3
+      .map((stuNo) => Student("Student number: $stuNo name", stuNo))
+      .toList(); //with fat arrow
 
-  studentsNo.forEach((currentStudentNo) => print("Student No: $currentStudentNo"));
-  studentsNo2.forEach((currentStudentNo2) => print("Student No2: $currentStudentNo2"));  
-  studentsNo3.forEach((currentStudentNo3) => print("Student No3: $currentStudentNo3"));
+  studentsNo
+      .forEach((currentStudentNo) => print("Student No: $currentStudentNo"));
+  studentsNo2
+      .forEach((currentStudentNo2) => print("Student No2: $currentStudentNo2"));
+  studentsNo3
+      .forEach((currentStudentNo3) => print("Student No3: $currentStudentNo3"));
+
+  allStudents.forEach(
+      (currentStudent3) => print("Student name: ${currentStudent3.name}"));
 }
 
-int randomNo(){
+int randomNo() {
   int createdRandomNo = Random().nextInt(60);
 
-  if(createdRandomNo != 0 ){
+  if (createdRandomNo != 0) {
     return createdRandomNo;
-  }else{
+  } else {
     int newNo = randomNo();
     return newNo;
   }
 }
 
-class Student{
+class Student {
   String name;
   int no;
 
   Student(this.name, this.no);
+
+  @override
+  String toString() {
+    return "$name and $no"; //mapping short way
+  }
 }
