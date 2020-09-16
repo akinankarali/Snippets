69f7f8ded7d75b952b74a3d024b3561084c8f78f
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-09-16 15:45:59 +0300

    Added column and row and styling

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -28,12 +28,139 @@ void main() {
           ),
         ),
         body: Container(
-          color: Colors.purple,
-          child: Center(
-            child: Text(
-              "Flutter Amazing!",
-              textAlign: TextAlign.center,
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    color: Colors.red.shade200,
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.all(2),
+                    child: Text(
+                      "D",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red.shade300,
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.all(2),
+                    child: Text(
+                      "A",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red.shade600,
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.all(2),
+                    child: Text(
+                      "R",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red.shade700,
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.all(2),
+                    child: Text(
+                      "T",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.red.shade200,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "E",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red.shade300,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "R",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red.shade600,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "S",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red.shade700,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "L",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red.shade200,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "E",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red.shade300,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "R",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red.shade600,
+                padding: EdgeInsets.fromLTRB(24, 20, 24, 20),
+                margin: EdgeInsets.all(2),
+                child: Text(
+                  "I",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
